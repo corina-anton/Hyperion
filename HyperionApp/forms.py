e9cc967c8afbc7f33356364f56615baba857e55f
Corina Anton <corinamihaelaan@gmail.com>
2021-09-14 17:38:11 +0100

    Improved sign-up validation (moved conditions in forms.py for a better structure, added error handling in signup.html)

--- HyperionApp/forms.py
+++ HyperionApp/forms.py
@@ -13,6 +13,35 @@ class signup_form(forms.Form):
     password = forms.CharField(max_length=254)
     confirm_password = forms.CharField(max_length=254)
 
+    # Verify if there already is an account that uses the same email;
+    def clean_email(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get("email")
+
+        user = User.objects.all().filter(email=email)
+
+        if len(user) > 0:
+            raise forms.ValidationError("There already is an account that uses this email")
+        else:
+            return email
+
+    # Verify if the password and confirm password fields match;
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError("Passwords do not match")
+        else:
+            return cleaned_data
+
+        # The 'def clean_password' method does cleaning specific to that particular attribute.
+        # The 'def clean()' method performs validation that requires access to multiple form fields.
+        # Here you must use `def clean(self)` and not `def clean_password`.
+        # This is because the order of 'cleaning' matters.
+        # If you use `def clean_password`, django will only clean the 'password' input BUT you need the 'confirm_password' as well which not yet cleaned.
+
 class login_form(forms.Form):
     email= forms.EmailField(label='Email', max_length=254)
     password = forms.CharField(max_length=254)
--- HyperionApp/views.py
+++ HyperionApp/views.py
@@ -4,54 +4,35 @@ from .forms import signup_form
 from .forms import login_form
 from django import forms
 from .models import User
+from django.contrib.auth.hashers import make_password, check_password
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-    if request.method=='GET':
+    if request.method == 'GET':
         return render(request, 'signup.html')
 
-    elif request.method=='POST':
+    elif request.method == 'POST':
 
-        # Retrieve the data sent by the use through HTTP:
+        return HttpResponse(request)
         signup_instance = signup_form(request.POST)
 
-        # Input validation:
+
         if not signup_instance.is_valid():
-            return HttpResponse("error")
+            return render(request, 'signup.html', context={'errors': signup_instance.errors.values()})
 
-        # Part of input validation: Verify whether the email already exists in the database
-        email = signup_instance.cleaned_data['email']
-        db_email = User.objects.all().filter(email=email)
-
-        if len(db_email) > 0:
-            return HttpResponse("There is another account created with this email")
-
-        # Part of input validation: Verify if confirm_password and password inputs are identical
-        password = signup_instance.cleaned_data['password']
-        confirm_password = signup_instance.cleaned_data['confirm_password']
-
-        if password != confirm_password:
-            return HttpResponse("error")
-
-        # Send to db
         else:
             title = signup_instance.cleaned_data['title']
             first_name = signup_instance.cleaned_data['first_name']
             last_name = signup_instance.cleaned_data['last_name']
 
-            # Concatenate dob
             dob_concatenated = signup_instance.cleaned_data['dob_day'] + "/" + signup_instance.cleaned_data['dob_month'] + "/" + signup_instance.cleaned_data['dob_year']
 
-            email = signup_instance.cleaned_data['email']
+            encrypted_password = make_password(password)
 
-            # Encrypt password
-            password = make_password(signup_instance.cleaned_data['password'])
-
-            # Add to db
-            add_db = User(title=title, first_name=first_name, last_name=last_name, dob=dob_concatenated, email=email, password=password)
+            add_db = User(title=title, first_name=first_name, last_name=last_name, dob=dob_concatenated, email=email, password=encrypted_password)
             add_db.save()
 
             return HttpResponse("thank you")
@@ -65,7 +46,8 @@ def login(request):
         # Create an instance of your login_form
         login_instance = login_form(request.POST)
 
+        # Input validation (it triggers the conditions written in forms.py)
         if not login_instance.is_valid():
-            return render(request, 'login.html', context={'errors': login_instance.errors.values() })
+            return render(request, 'login.html', context={'errors': login_instance.errors.values()})
 
         return render(request, 'dashboard.html')
